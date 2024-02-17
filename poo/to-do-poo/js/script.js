4d43b002223cab7157d0ce51147ef14d3b1e97fb
Adrian <adrian.dryzara3@gmail.com>
2024-02-16 20:05:22 -0400

    metodos basicos creados

--- poo/to-do-poo/js/script.js
+++ poo/to-do-poo/js/script.js
@@ -1,5 +1,6 @@
 var id = 0
 let tasks = []
+let error = document.getElementById("error")
 
 class Task {
     constructor(id, title, description){
@@ -7,28 +8,48 @@ class Task {
         this.title = title
         this.description = description
     }
-}
 
-function createTask(title, description){
-    let task = new Task(id, title, description)
-    id += 1
-    tasks.push(task)
-    return task
-}
-
-let task = createTask("task1", "task to do")
-console.log(task)
-
-let task2 = createTask("task2", "task to do")
-console.log(task2)
-
-console.log(task)
-console.log(task2)
-
-
-tasks.forEach((task, i) => {
-    console.log(`Tarea ${i}`)
-    for (const prop in task) {
-        console.log(task[prop])
+    updateTasks(task, title, description){
+        task.title = title
+        task.description = description
     }
-});
+}
+
+function newTask(){
+    error.innerHTML = '' 
+    let title = document.getElementById('taskTitle')
+    let description = document.getElementById('taskDesc')
+    if (!title.value && !description.value) {
+        error.innerHTML = "Por Favor agregue una tarea!"
+        return
+    }else {
+        let task = new Task(id, title.value, description.value)
+        id += 1
+        tasks.push(task)
+        title.value = ''
+        description.value = ''
+        updateTasks();
+        return task
+    }
+}
+
+function updateTasks() {
+    let taskList = document.getElementById("tareas");
+    taskList.innerHTML = "";
+    tasks.forEach((task, index) => {
+        let newTask = `
+        <li class="list-group-item">
+            <label class="form-check-label">${task.title}:<br> ${task.description}</label>
+            <button onclick="deleteTask(${index})" class="btn btn-warning float-right">Eliminar Tarea</button>
+            <button class="btn btn-secondary float-right mr-4">Actualizar Tarea</button>
+        </li>
+
+        `;
+        taskList.innerHTML += newTask;
+    });
+}
+
+function deleteTask(i){
+    tasks.splice(i, 1) 
+    updateTasks();
+}
